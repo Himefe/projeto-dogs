3bc01a1b0caf951a285d9b65968347d4d3e12cdb
Himefe <idanger2015@hotmail.com>
2022-02-03 15:04:24 -0300

    Tentando resolver problemas com scroll no celular

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -14,7 +14,7 @@ const Feed = ({ userID }) => {
       if (infinite) {
         const scroll = window.scrollY;
         const height = document.body.offsetHeight - window.innerHeight;
-        if (scroll === height && wait === false) {
+        if (scroll > height * 0.75 && !wait) {
           setPages((pages) => [...pages, pages.length + 1]);
           wait = true;
           setTimeout(() => {
