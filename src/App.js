6f7ec4880d9ce8e12c3cdbd6a47e725e25973fa6
Himefe <idanger2015@hotmail.com>
2022-02-03 12:08:06 -0300

    Finalização do projeto dogs

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import Home from "./Components/Home";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
 import "./index.css";
 import { UserStorage } from "./UserContext";
+import ProtectedRouter from "./Components/Helper/ProtectedRouter";
+import Photo from "./Components/Feed/Photo/Photo";
+import NotFound404 from "./Components/Helper/NotFound404";
+import Perfil from "./Components/Feed/Perfil/Perfil";
 
 function App() {
   return (
@@ -15,7 +20,18 @@ function App() {
           <main>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/login/*" element={<Login />} />
+              <Route path="login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRouter>
+                    <User />
+                  </ProtectedRouter>
+                }
+              />
+              <Route path="/foto/:id" element={<Photo />} />
+              <Route path="/*" element={<NotFound404 />} />
+              <Route path="/perfil/:author" element={<Perfil />} />
             </Routes>
           </main>
           <Footer />
